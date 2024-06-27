35ad3b0b8551a49cf3fcc15028a742d08a6cc295
Umang Hirani <umanghirani.exe@gmail.com>
2024-06-27 15:59:42 +0530

    all pages added

--- hcm-react/src/App.jsx
+++ hcm-react/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from './components/Container';
 import Input from './components/Input';
+import Settings from './components/Settings';
+import Account from './components/Account';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
         <Route exact path='/' element={<Container />} />
         <Route exact path='/details' element={<Input />} />
         <Route exact path='/checkup' element={<Input />} />
+        <Route exact path='/settings' element={<Settings />} />
+        <Route exact path='/profile' element={<Account/>} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
